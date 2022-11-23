f65224b875985e6f455ee0343bac414d74455575
James Rider <j.rider@utah.edu>
2022-11-22 21:50:44 -0700

    another update to run.sh

--- SVM/run.sh
+++ SVM/run.sh
@@ -6,6 +6,14 @@ pip install matplotlib
 pip install numpy
 pip install scipy
 
-python driver.py
+python driver.py primal
+
+echo "Running Dual: This will take a moment"
+
+python driver.py dual
+
+echo "Running Gaussian Kernel: This will take a moment"
+
+python driver.py gk
 
 read
